feef2c3d3f02a0874210913d69b9f4cbddde357c
kosyakoff1 <kosyakoff1@gmail.com>
2018-01-13 21:00:28 +0700

    First positive unit test

--- Starter/FriendStorage/UiTests/Viewmodel/NavigationViewmodelTests.cs
+++ Starter/FriendStorage/UiTests/Viewmodel/NavigationViewmodelTests.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FriendStorage.UI.ViewModel;
+using Xunit;
 
 namespace UiTests.Viewmodel
 {
 
     public class NavigationViewmodelTests
     {
+        [Fact]
+        public void ShouldLoadFriends()
+        {
+            //Arrange
+            var navigationViewModel = new NavigationViewModel();
+
+            //Act
+            navigationViewModel.LoadFriends();
+
+            //Assert
+            Assert.Equal(8,navigationViewModel.Friends.Count); 
+        }
     }
 }
